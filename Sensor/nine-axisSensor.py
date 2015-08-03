fa398d7a8df08f425fb62b835d627ed8fe88c30c
noblesseoblige6 <s1180123@gmail.com>
2015-08-03 16:17:13 +0900

    add test file to get gyro values

--- Sensor/nine-axisSensor.py
+++ Sensor/nine-axisSensor.py
@@ -1,7 +1,7 @@
 import smbus
 import time
 
-def mergeHL(h, l):
+def convertAccel(h, l):
   out = 0
   if h & 0b10000000 == 0b10000000:
     val = (h<<8) | l
@@ -10,31 +10,48 @@ def mergeHL(h, l):
     out = val_inv*(-1)
   else:
     out = (h<<8) | l
-  return out/256.0 
-  
+
+  # return 2.0*out/32768.0
+  return out
+
+
+def convertGyro(h, l):
+  out = 0
+  if h & 0b10000000 == 0b10000000:
+    val = (h<<8) | l
+    val_inv = val^0b1111111111111111
+    val_inv = val_inv+1
+    out = val_inv*(-1)
+  else:
+    out = (h<<8) | l
+
+  # return 250.0*out/32768.0
+  return out
 
 #@comment Initialize
 busNumber = 1
 addr = 0x68
 bus = smbus.SMBus(busNumber)
 #@comment Wake up sensors
-bus.write_byte_data(addr, 0x6b, 0x00)
-bus.write_byte_data(addr, 0x6c, 0x00)
-
-# bus.write_byte_data(addr, 0x1b, 0x00)
-bus.write_byte_data(addr, 0x1b, 0x18)
-bus.write_byte_data(addr, 0x1c, 0x18)
+bus.write_byte_data(addr, 0x6b, 0b00000000)
+bus.write_byte_data(addr, 0x6c, 0b00000000)
+#@comment config gyro
+bus.write_byte_data(addr, 0x1b, 0x00)
+# bus.write_byte_data(addr, 0x1b, 0b11100000)
+#@comment accel
+# bus.write_byte_data(addr, 0x1c, 0b11100000)
+bus.write_byte_data(addr, 0x1c, 0x00)
 
 while True:
   accels = bus.read_i2c_block_data(addr, 0x3b, 6)
-  accelX = mergeHL(accels[0], accels[1])
-  accelY = mergeHL(accels[2], accels[3])
-  accelZ = mergeHL(accels[4], accels[5])
+  accelX = convertAccel(accels[0], accels[1])
+  accelY = convertAccel(accels[2], accels[3])
+  accelZ = convertAccel(accels[4], accels[5])
 
   gyoros = bus.read_i2c_block_data(addr, 0x43, 6)
-  gyoroX = mergeHL(gyoros[0], gyoros[1])
-  gyoroY = mergeHL(gyoros[2], gyoros[3])
-  gyoroZ = mergeHL(gyoros[4], gyoros[5])
+  gyoroX = convertGyro(gyoros[0], gyoros[1])
+  gyoroY = convertGyro(gyoros[2], gyoros[3])
+  gyoroZ = convertGyro(gyoros[4], gyoros[5])
 
   print accelX, accelY, accelZ
   print gyoroX, gyoroY, gyoroZ
